caac182ea156b633fa7f1c6a6da22c6a43135413
ct1150 <ct1150@gmail.com>
2020-06-03 15:04:45 +0800

    ensure authResponseHeaders is not null

--- src/handler.lua
+++ src/handler.lua
@@ -32,10 +32,11 @@ function AuthForwardHandler:access(conf)
   if res.status ~= 200 then
     return kong.response.exit(401,"auth fail")
   end
- 
-  for k,v in pairs(conf.authResponseHeaders)
-  do
-    kong.service.request.set_header(tostring(v),res.headers[tostring(v)])
+  if conf.authResponseHeaders then
+    for k,v in pairs(conf.authResponseHeaders)
+    do
+      kong.service.request.set_header(tostring(v),res.headers[tostring(v)])
+	end
   end
  
 end
