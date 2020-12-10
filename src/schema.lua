4ca31dd232de6fc4f4ad298ed44ee1a1b5af62bd
ct1150 <ct1150@gmail.com>
2020-12-10 10:11:58 +0800

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -4,8 +4,8 @@ return {
     url = { required = true, type = "url" },
     path = { required = true, type = "string" },
     authResponseHeaders = { type = "array" },
-    connect_timeout = { default = 10000, type = "number" },
-    send_timeout = { default = 60000, type = "number" },
-    read_timeout = { default = 60000, type = "number" }
+    connect_timeout = { default = 30000, type = "number" },
+    send_timeout = { default = 30000, type = "number" },
+    read_timeout = { default = 30000, type = "number" }
   }
 }
